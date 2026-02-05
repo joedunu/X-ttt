2d1de937744368856a4ae80ef8e15d1e29dbabde
Anuradha Dunuhappawa <joedunu@gmail.com>
2026-02-05 23:35:11 +1100

    Register leaderboard route

--- WS/Xttt.js
+++ WS/Xttt.js
@@ -6,6 +6,8 @@ io = require('socket.io')(server);
 
 util = require("util");							// Utility resources (logging, object inspection, etc)
 
+var Leaderboard = require('./Leaderboard');		// Leaderboard module
+
 /**************************************************
 ** GAME VARIABLES
 **************************************************/
@@ -13,6 +15,15 @@ Player = require("./Player").Player;			// Player class
 players = [];									// Array of connected players
 players_avail = [];
 
+// Routing
+app.use(express.static(__dirname + '/public'));
+
+// API: Get leaderboard
+app.get('/api/leaderboard', function(req, res) {
+	var limit = parseInt(req.query.limit, 10) || 10;
+	var topPlayers = Leaderboard.getTopPlayers(limit);
+	res.json(topPlayers);
+});
 
 var port = process.env.PORT || 3001;
 
@@ -20,8 +31,6 @@ server.listen(port, function () {
 	console.log('Server listening at port %d', port);
 });
 
-// Routing
-app.use(express.static(__dirname + '/public'));
 
 require('./XtttGame.js');
 
